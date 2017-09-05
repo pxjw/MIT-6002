374c9e165250fe3b4ab6ba07b25bcf23f4fb8a26
miral <pengjunweiright@163.com>
2017-09-05 22:06:17 +0800

    cose 1.1

---
+++ keywordex.py
@@ -0,0 +1,21 @@
+# def lotsOfParameters1(a,b,c,d,e):
+#     print (a)
+#     print (b)
+#     print (c)
+#     print (d)
+#     print (e)
+#
+# #lotsOfParameters1()
+# #lotsOfParameters1(1,2)
+# #lotsOfParameters1(1,e=5,d=4,c=3,b=2)
+# #lotsOfParameters1(a=5, b=1, c=4, d=2, 3)
+# #lotsOfParameters1(e=5,a=1,d=4,b=2,c=3)
+
+def lotsOfParameters2(a=1,b=2,c=3,d=4,e=5):
+    print (a)
+    print (b)
+    print (c)
+    print (d)
+    print (e)
+
+# lotsOfParameters2(1,c=2)
